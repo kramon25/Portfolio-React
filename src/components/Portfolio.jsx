f131a048f95e0ff9652abca5f7f73448589a4a25
unknown <ramondokenny585@gmail.com>
2023-10-30 18:02:15 -0400

    project descriptions added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -29,7 +29,7 @@ export default function Portfolio() {
               imgPath={travel}
               isBlog={false}
               title="TravelPlanner"
-              description=""
+              description="A Travel Planning application that allows you to search any destination and it will find you flights, transportation options, recommend accommodations, and nearby restraunts. Utilizing multiple api-keys."
               ghLink="https://github.com/BLam1226/Travel-Planner"
             />
             <button className="portBtn">
@@ -47,7 +47,7 @@ export default function Portfolio() {
               imgPath={omnifood}
               isBlog={false}
               title="Omnifood"
-              description=""
+              description="A basic, html/css and a hint of javascript, application that has fully functioning navbar and buttons for desktop and mobile devices."
               ghLink="https://github.com/kramon25/omnifood"
             />
             <button className="portBtn">
@@ -65,7 +65,7 @@ export default function Portfolio() {
               imgPath={weather}
               isBlog={false}
               title="WeatherApp"
-              description=""
+              description="Weather application that calls data from a weather api. Displaying data from that api and call code from the local storage to save to the recent search history that collects underneath the search bar."
               ghLink="https://github.com/kramon25/Weather-App"
             />
             <button className="portBtn">
@@ -83,7 +83,7 @@ export default function Portfolio() {
               imgPath={password}
               isBlog={false}
               title="Password Generator"
-              description=""
+              description="A password generator created with html, css and javascript. My job was to create the javascript. A number of prompts will appear and the user will type what they have to and select yes or no when given the option. If the user fails to do so they will recieve an alert."
               ghLink="https://github.com/kramon25/Kenny-password-generator"
             />
             <button className="portBtn">
@@ -101,7 +101,7 @@ export default function Portfolio() {
               imgPath={notetaker}
               isBlog={false}
               title="Heroku App Note Taker"
-              description=""
+              description="This is an application built to allow users to save and take notes online utilizing node.js, html, css and javascript."
               ghLink="https://github.com/kramon25/Note-Taker"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
@@ -120,7 +120,7 @@ export default function Portfolio() {
               imgPath={calendar}
               isBlog={false}
               title="Workday Schedule"
-              description=""
+              description="A functional workday calendar that would save the content to the local storage using the save button. The javascript created runs through the current time of day and highlights green for future times, red for present annd grey for past."
               ghLink="https://github.com/kramon25/Calendar"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
             />
