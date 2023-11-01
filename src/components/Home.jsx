997480bf97091b22e50b5844042634a9b30c95cc
unknown <ramondokenny585@gmail.com>
2023-11-01 15:42:27 -0400

    got avatar to stop moving with typewriter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,6 @@ export default function Home() {
               <Typewriter
                 options={{
                   strings: [
-                    "FULLSTACK WEB DEVELOPER",
                     "HTML",
                     "CSS",
                     "JAVASCRIPT",
